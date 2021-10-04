cd109590e5329465722433fc41751d264fa2c65a
farid <f.putrainsan@gmail.com>
2021-10-04 09:28:30 +0700

    add rom esp test

---
+++ test_ext_epprom_esp/test_ext_epprom_esp.ino
@@ -0,0 +1,60 @@
+#include <Wire.h>
+
+//1. Alamat = 0x57
+
+#define ADD_I2C 0x57
+
+#define pinSDA D5
+#define pinSCL D6
+
+void setup() {
+
+  Serial.begin(9600);
+
+  Wire.begin(pinSDA, pinSCL);
+
+  delay(100);
+
+  writeData(0, 12);
+  Serial.println(readData( 1));
+
+
+
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+void writeData(byte address, byte data) {
+
+  Wire.beginTransmission(ADD_I2C);
+  Wire.write(address);
+  Wire.write(data);
+  Serial.print("write Data  ");
+  Serial.println(data);
+  Wire.endTransmission();
+  delay(5);
+}
+
+
+byte readData(byte address) {
+  byte data ;
+  Wire.beginTransmission(ADD_I2C);
+  Wire.write(address);
+  Wire.endTransmission();
+  Wire.requestFrom(ADD_I2C, 1);
+    delay(5);
+  if (Wire.available()) {
+    Serial.print("read data : ");
+    data = Wire.read();
+    Serial.println(data);
+    delay(5);
+  }
+  Wire.endTransmission();
+  
+  return data;
+}
