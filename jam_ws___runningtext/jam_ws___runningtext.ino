7f05ec579cf29859c77a2a69f80391dea4eeec5c
faritputrainsan <f.putrainsan@gmail.com>
2022-03-29 19:58:53 +0700

    edit text

--- jam_ws___runningtext/jam_ws___runningtext.ino
+++ jam_ws___runningtext/jam_ws___runningtext.ino
@@ -10,19 +10,14 @@
  */
 
 
-
-
 #include <SPI.h>
 #include <avr/pgmspace.h>
 #include "HUB08SPI.h"
 #include "TimerOne.h"
 #include "Buffer.h"
 
-
-
-#define WIDTH 64 // panjang led matrix
+#define WIDTH  64// panjang led matrix
 #define HEIGHT 16 // tinggi led matrix
-
 
 HUB08SPI display;
 
@@ -38,7 +33,7 @@ void refresh(){
 void setup() {
 
   Serial.begin(9600);
-  display.begin(displaybuf,WIDTH, HEIGH);
+  display.begin(displaybuf,WIDTH, HEIGHT);
   Timer1.initialize(250);
   Timer1.attachInterrupt(refresh);
   display.setBrightness(2000);
@@ -48,12 +43,42 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+// runningText("test Run");
+jadwal();
+//ronn.setFont(B_STD);
+// ronn.printText("Animation Demo",0,0);delay(500);
+// ronn.clearSlice_R(0,8,64,16);
 }
 
 
-void runningText(String TEXT){
-  
+void runningText(char* TEXT){
+  ronn.setFont(B_STD);
+  ronn.scrollText_LR(TEXT, 0, 0, WIDTH, 20);
 }
 
-S
+void jadwal(){
+
+//   ronn.clear_U(0,8,64,8,CLEAR);delay(300);
+//    ronn.scrollText_U("06:30:55",11,8,64-11);
+//    delay(1000-160);
+//    for(int i=6;i<=9;i++){
+//      ronn.scrollText_U(String(i),36+11,8,5);
+//      delay(1000-160);
+//    }
+//    ronn.scrollText_U("00",30+11,8,11);
+//    ronn.scrollText_U("1",21+11,8,5);
+
+    
+//    ronn.clear_U();
+    
+  ronn.clear_D();
+  ronn.setFont(B_7SEGMENT);
+  ronn.scrollText_D("06:30", 9, 0, 64 - 11);
+  for (int i = 1; i <= 5; i++) {
+    delay(400);
+    buff.fillRect(31, 0, 2, 8, 0);
+    delay(600);
+    ronn.printText(":", 31, 0);
+  }
+//  ronn.clear_R();
+}
