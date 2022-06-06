946d5f2c5a693eb726bd2becd30e37c0d58a9fe7
faritputrainsan <f.putrainsan@gmail.com>
2022-06-06 19:29:35 +0700

    external eeprom ok

--- receive_exeeprom/receive_exeeprom.ino
+++ receive_exeeprom/receive_exeeprom.ino
@@ -1,14 +1,20 @@
 #include <Wire.h>
+#include <I2C_eeprom.h>
 
-//1. Alamat = 0x57
+
+
+//1. Alamat 24LC32 = 0x57
 
 #define ADD_I2C 0x57
+
+//Device identifier
+I2C_eeprom eeprom(ADD_I2C, I2C_DEVICESIZE_24LC32);
 
 int input = 9;
 byte r_input;
 
-String textData = "Test external eeprom";
-String TEXT_ABC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+int lenText = 256;
+
 String textSerial;
 
 //#define pinSDA D5
@@ -17,7 +23,13 @@ String textSerial;
 void setup() {
 
   Serial.begin(9600);
-  Wire.begin();
+
+  eeprom.begin();
+  if (!eeprom.isConnected()) {
+    Serial.print("ERROR: can't find eeprom\nstoped");
+    while (1);
+  }
+  //  Wire.begin();
   delay(100);
 
   pinMode (input, INPUT);
@@ -28,12 +40,12 @@ void loop() {
   r_input = digitalRead(input);
 
   if (r_input == LOW) {
-    writetxt(1, TEXT_ABC);
-    
-//    Serial.println(readData(1));
+    //    writetxt(1, TEXT_ABC);
+
+    //    Serial.println(readData(1));
     delay(500);
 
-    Serial.print(readText(1));
+    //    Serial.print(readText(1));
     delay (500);
   }
 }
@@ -41,77 +53,95 @@ void loop() {
 char Ch_prm[250];
 
 void serialEvent() {
-    char data;
-    int index = 0;
-    
-    if(Serial.available()){
-      textSerial = Serial.readString();
-      delay(100);
-      writetxt(0, textSerial);
-    }
 
+  if (Serial.available()) {
+    textSerial = Serial.readString();
+    delay(100);
+    writetxt(0,    textSerial,      lenText);
+    /////(address, text data, panjang arrays)
     delay(1000);
-    Serial.print(readText(1));
-//    if (textSerial.substring(0,1) == "T"){
-//     
-//    }
+  }
 }
 
-void writetxt(unsigned int addres, String text) {
+void writetxt(unsigned int addres, String text, int length) {
   //  String text;
 
-  for (int i = 0; i < text.length(); i++ ) {
-    writeEEprom(addres, text[i]);
-    Serial.println(text[i]);
-    addres++;
-    delay(100);
+  //  Set block plotter on eeprom use library
+  eeprom.setBlock(addres, 0, length);
+  /////////////////////////////////////
+
+  // create String to char arrays
+  int len = text.length() + 1;
+  char data2[len];
+  text.toCharArray(data2, len);
+  ////////////////////////////
+
+  //  char to read data array from eeprom
+  char data[length];
+  ////////////////////////////////////////
+
+
+  /////////////////(address,    data                ,   length text)
+  eeprom.writeBlock(addres      , (uint8_t *) &data2, sizeof(data2));
+  //////////////////////////////////////////////////////////////////
+
+  //  read data from eeprom using for loop
+  for (int i = 0; i < length; i++) {
+    //    if (i % 10 == 0 ) SERIAL_OUT.println();
+    //    SERIAL_OUT.print(' ');
+    data[i] = eeprom.readByte(i);
+    //    SERIAL_OUT.print((char)ee.readByte(i));
   }
-  writeEEprom(addres, '\n');
-  delay(5);
+  ////////////////////////////////////////
 
+  //char array to data string
+  
+  Serial.println((String)data);
 }
 
-void writeEEprom(unsigned int address, int data) {
-  wireTransmission(address);
-  Wire.write(data);
-  Serial.print("write Data  ");
-//  Serial.println(data,HEX);
-  Serial.println(data);
-  Wire.endTransmission();
-  delay(5);
-}
+//manual access external eeprom using build in Wire lib
 
-void  wireTransmission(unsigned int address) {
-  Wire.beginTransmission(ADD_I2C);
-  Wire.write((int)(address >> 8));   //MSB
-  Wire.write((int)(address & 0xFF)); //LSB
-}
-
-int readEEprom(unsigned int address) {
-  byte data ;
-  wireTransmission(address);
-  Wire.endTransmission();
-  Wire.requestFrom(ADD_I2C, 1);
-  delay(5);
-  if (Wire.available()) {
-    data = Wire.read();
-    Serial.print("read data : ");
-    Serial.println(data);
-    delay(5);
-  }
-  return data;
-}
-
-String readText(int addrss){
-  int strLen = readEEprom(addrss);
-  char readTxt[strLen];
-  int i = 0;
-  while(i < strLen){
-    readTxt[i] = readEEprom(addrss + i);
-    i++;
-    if (readEEprom(addrss + i) == '\n'){
-      break;
-    }
-  }
-  return String (readTxt);
-}
+//void writeEEprom(unsigned int address, int data) {
+//  wireTransmission(address);
+//  Wire.write(data);
+//  Serial.print("write Data  ");
+//  //  Serial.println(data,HEX);
+//  Serial.println(data);
+//  Wire.endTransmission();
+//  delay(5);
+//}
+//
+//void  wireTransmission(unsigned int address) {
+//  Wire.beginTransmission(ADD_I2C);
+//  Wire.write((int)(address >> 8));   //MSB
+//  Wire.write((int)(address & 0xFF)); //LSB
+//}
+//
+//int readEEprom(unsigned int address) {
+//  byte data ;
+//  wireTransmission(address);
+//  Wire.endTransmission();
+//  Wire.requestFrom(ADD_I2C, 1);
+//  delay(5);
+//  if (Wire.available()) {
+//    data = Wire.read();
+//    Serial.print("read data : ");
+//    Serial.println(data);
+//    delay(5);
+//  }
+//  return data;
+//}
+//
+//String readText(int addrss) {
+//  int strLen = readEEprom(addrss);
+//  char readTxt[strLen];
+//  int i = 0;
+//  while (i < strLen) {
+//    readTxt[i] = readEEprom(addrss + i);
+//    i++;
+//    if (readEEprom(addrss + i) == '\0') {
+//      break;
+//    }
+//  }
+//  return String (readTxt);
+//}
